5bd15c66db9f1d7c6a9f9a03fe6c2027dd6927ea
Kiju Saijou <ksaijou@ksaijou-ltm.internal.salesforce.com>
2018-07-17 12:48:23 +0900

    Add recomendation of Product from Marketing Cloud
    - Add recomendation js
    - Add recomendation tag to Top page
    - Add style for recomedation

---
+++ cartridges/app_custom/cartridge/static/default/js/app_custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    // MC recommend jsonp
+    // $.ajax({
+    //     url: 'https://1454285.recs.igodigital.com/a/v2/1454285/home/recommend.json',
+    //     dataType: 'jsonp',
+    // })
+    // .done(function(data) {
+    //     console.log(data);
+    // });
+});
