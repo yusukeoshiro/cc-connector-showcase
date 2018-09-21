2dbc0d32840a99651ee700a11b20a2834cb8d12b
nyamaura <nyamaura@nyamaura-ltm.internal.salesforce.com>
2018-09-21 15:56:22 +0900

    Upload cartridges for NTO first time.

---
+++ cartridges/nto_sitegenesis_core/cartridge/js/pages/wishlist.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var addProductToCart = require('./product/addToCart'),
+    page = require('../page'),
+    util = require('../util');
+
+exports.init = function () {
+    addProductToCart();
+    $('#editAddress').on('change', function () {
+        page.redirect(util.appendParamToURL(Urls.wishlistAddress, 'AddressID', $(this).val()));
+    });
+
+    //add js logic to remove the , from the qty feild to pass regex expression on client side
+    $('.option-quantity-desired input').on('focusout', function () {
+        $(this).val($(this).val().replace(',', ''));
+    });
+};
